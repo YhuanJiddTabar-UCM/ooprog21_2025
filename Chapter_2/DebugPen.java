b611b373fb9b1f7b488b8a3ac3cea897d9500099
YhuanJiddTabar-UCM <yhuantabar2018@gmail.com>
2025-09-26 09:08:37 +0800

    Add files via upload

---
+++ Chapter_2/DebugPen.java
@@ -0,0 +1,26 @@
+public class DebugPen {
+    private String color;
+    private String point;
+
+    
+    public DebugPen() {
+        color = "black";
+        point = "fine";
+    }
+
+    // constructor with parameter
+    public DebugPen(String color, String point) {
+        this.color = color;   // originally was color = color; fixed into this.color = color;  
+        this.point = point;   // originally was point = point; fixed into this.point = point;  
+    }
+
+    //Getter 
+    public String getColor() {
+        return color;
+    }
+
+    
+    public String getPoint() {
+        return point;
+    }
+}
