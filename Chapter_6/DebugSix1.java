68f43393d63d5a6634a7c97454e7276ef4233da5
YhuanJiddTabar-UCM <yhuantabar2018@gmail.com>
2025-10-24 10:32:59 +0800

    Update DebugSix1.java

--- Chapter_6/DebugSix1.java
+++ Chapter_6/DebugSix1.java
@@ -10,16 +10,16 @@ import java.util.Scanner;
         Scanner keyboard = new Scanner(System.in);
         int days;
         double money = 0.01;
-        int day = 0;//initialized day to 0
-        
+        int day = 1;        
         System.out.print("Enter number of days >> ");  //added the missing semicolon
         days = keyboard.nextInt();
 
-        while(days > day) {  // operator changed, from < to >
+        while(days >= day) {  // operator changed, from < to >=
             
             money = 2 * money;
-            ++day ;
             System.out.println("After day " + day + " you have " + money);  //wrong variable in message
+            ++day ;
+
 
         }
     }
