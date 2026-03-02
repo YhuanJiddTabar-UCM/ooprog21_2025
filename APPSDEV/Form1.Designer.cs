b1254186ee834f363da419405265273b1ec35d19
YhuanJiddTabar-UCM <yhuantabar2018@gmail.com>
2026-03-02 09:44:23 +0800

    Add files via upload

---
+++ APPSDEV/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Currency
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lb11 = new System.Windows.Forms.Label();
+            this.lb10 = new System.Windows.Forms.Label();
+            this.lb9 = new System.Windows.Forms.Label();
+            this.lb8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cb2 = new System.Windows.Forms.ComboBox();
+            this.cb1 = new System.Windows.Forms.ComboBox();
+            this.tb1 = new System.Windows.Forms.RichTextBox();
+            this.ConvertButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(295, 371);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Simple Currency Converter";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lb11);
+            this.groupBox3.Controls.Add(this.lb10);
+            this.groupBox3.Controls.Add(this.lb9);
+            this.groupBox3.Controls.Add(this.lb8);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Location = new System.Drawing.Point(18, 199);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(259, 147);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Output";
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // lb11
+            // 
+            this.lb11.AutoSize = true;
+            this.lb11.Location = new System.Drawing.Point(146, 112);
+            this.lb11.Name = "lb11";
+            this.lb11.Size = new System.Drawing.Size(0, 13);
+            this.lb11.TabIndex = 9;
+            // 
+            // lb10
+            // 
+            this.lb10.AutoSize = true;
+            this.lb10.Location = new System.Drawing.Point(146, 84);
+            this.lb10.Name = "lb10";
+            this.lb10.Size = new System.Drawing.Size(0, 13);
+            this.lb10.TabIndex = 8;
+            // 
+            // lb9
+            // 
+            this.lb9.AutoSize = true;
+            this.lb9.Location = new System.Drawing.Point(146, 55);
+            this.lb9.Name = "lb9";
+            this.lb9.Size = new System.Drawing.Size(0, 13);
+            this.lb9.TabIndex = 7;
+            // 
+            // lb8
+            // 
+            this.lb8.AutoSize = true;
+            this.lb8.Location = new System.Drawing.Point(146, 28);
+            this.lb8.Name = "lb8";
+            this.lb8.Size = new System.Drawing.Size(0, 13);
+            this.lb8.TabIndex = 6;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(17, 112);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(98, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Converted Amount:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(17, 84);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Currency To:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 55);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Currency From:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Amount:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.cb2);
+            this.groupBox2.Controls.Add(this.cb1);
+            this.groupBox2.Controls.Add(this.tb1);
+            this.groupBox2.Controls.Add(this.ConvertButton);
+            this.groupBox2.Location = new System.Drawing.Point(18, 28);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(259, 165);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Input";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Currency To:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Amount:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Currency From:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // cb2
+            // 
+            this.cb2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb2.FormattingEnabled = true;
+            this.cb2.Items.AddRange(new object[] {
+            "PHP",
+            "EUR",
+            "CAD",
+            "USD"});
+            this.cb2.Location = new System.Drawing.Point(122, 88);
+            this.cb2.Name = "cb2";
+            this.cb2.Size = new System.Drawing.Size(131, 21);
+            this.cb2.TabIndex = 3;
+            // 
+            // cb1
+            // 
+            this.cb1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb1.FormattingEnabled = true;
+            this.cb1.Items.AddRange(new object[] {
+            ""});
+            this.cb1.Location = new System.Drawing.Point(122, 61);
+            this.cb1.Name = "cb1";
+            this.cb1.Size = new System.Drawing.Size(131, 21);
+            this.cb1.TabIndex = 1;
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(122, 30);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(131, 25);
+            this.tb1.TabIndex = 2;
+            this.tb1.Text = "";
+            // 
+            // ConvertButton
+            // 
+            this.ConvertButton.Location = new System.Drawing.Point(69, 125);
+            this.ConvertButton.Name = "ConvertButton";
+            this.ConvertButton.Size = new System.Drawing.Size(112, 23);
+            this.ConvertButton.TabIndex = 1;
+            this.ConvertButton.Text = "Convert";
+            this.ConvertButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 410);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simple Currency Converter";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox cb2;
+        private System.Windows.Forms.ComboBox cb1;
+        private System.Windows.Forms.RichTextBox tb1;
+        private System.Windows.Forms.Button ConvertButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lb11;
+        private System.Windows.Forms.Label lb10;
+        private System.Windows.Forms.Label lb9;
+        private System.Windows.Forms.Label lb8;
+    }
+}
+
---
+++ APPSDEV/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Currency
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, double> rates = new Dictionary<string, double>()
+        {
+            { "USD", 1.0 },
+            { "PHP", 54.907 },
+            { "EUR", 0.92 },
+            { "CAD", 1.35 }
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Connect events (safe even if already connected)
+            cb1.SelectedIndexChanged += cb1_SelectedIndexChanged;
+            ConvertButton.Click += ConvertButton_Click;
+
+            LoadCurrencies();
+            UpdateSecondCombo(); // ensure cb2 loads on startup
+        }
+
+        private void LoadCurrencies()
+        {
+            cb1.Items.Clear();
+
+            foreach (var currency in rates.Keys)
+            {
+                cb1.Items.Add(currency);
+            }
+
+            if (cb1.Items.Count > 0)
+                cb1.SelectedIndex = 0;
+        }
+
+        private void UpdateSecondCombo()
+        {
+            if (cb1.SelectedItem == null) return;
+
+            string selected = cb1.SelectedItem.ToString();
+
+            cb2.Items.Clear();
+
+            foreach (var currency in rates.Keys)
+            {
+                if (currency != selected)
+                    cb2.Items.Add(currency);
+            }
+
+            if (cb2.Items.Count > 0)
+                cb2.SelectedIndex = 0;
+        }
+
+        private void cb1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateSecondCombo();
+        }
+
+        private void ConvertButton_Click(object sender, EventArgs e)
+        {
+            if (!double.TryParse(tb1.Text, out double amount))
+            {
+                MessageBox.Show("Enter a valid number.");
+                return;
+            }
+
+            if (cb1.SelectedItem == null || cb2.SelectedItem == null)
+            {
+                MessageBox.Show("Select currencies first.");
+                return;
+            }
+
+            string from = cb1.SelectedItem.ToString();
+            string to = cb2.SelectedItem.ToString();
+
+            double amountInUSD = amount / rates[from];
+            double converted = amountInUSD * rates[to];
+
+            lb8.Text = amount.ToString("N2");
+            lb9.Text = from;
+            lb10.Text = to;
+            lb11.Text = converted.ToString("N2") + " " + to;
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
